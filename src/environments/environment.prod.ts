57f46eb440ffb9a5462afb0a72bd515cfad87548
Mitsuki Aihara <aizu.s1230001@gmail.com>
2019-07-22 16:42:18 +0900

    Change firebase environment to aizu.aisharing firebase environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   config: {
-    apiKey: 'AIzaSyCc8JhHItCWOomukSiecw-oPI2He_4fLvM',
-    authDomain: 'aisharing.firebaseapp.com',
-    databaseURL: 'https://aisharing.firebaseio.com',
-    projectId: 'aisharing',
-    storageBucket: '',
-    messagingSenderId: '720026759911',
-    appId: '1:720026759911:web:aae136860227bd54'
+    apiKey: "AIzaSyDNCt1OJv7qei2_tx-PhwwCXaoWXmCkKmA",
+    authDomain: "aisharing-ac6cc.firebaseapp.com",
+    databaseURL: "https://aisharing-ac6cc.firebaseio.com",
+    projectId: "aisharing-ac6cc",
+    storageBucket: "aisharing-ac6cc.appspot.com",
+    messagingSenderId: "7764789708",
+    appId: "1:7764789708:web:da14efe00b14ef31"
   }
 };
