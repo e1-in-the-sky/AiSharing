a9de6f88a93566a3953d34e577679a5c641c18f7
Mitsuki Aihara <aizu.s1230001@gmail.com>
2019-07-23 01:22:35 +0900

    add input form in reservation post page

--- src/app/pages/reservation-post/reservation-post.page.ts
+++ src/app/pages/reservation-post/reservation-post.page.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reservation-post.page.scss'],
 })
 export class ReservationPostPage implements OnInit {
+  data: { departure: string, destination: string, departure_time: string, comment: string }
+    = { departure: '', destination: '', departure_time: '', comment: 'よろしくお願いします。' };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onPost() {
+    console.log(this.data);
   }
 
 }
