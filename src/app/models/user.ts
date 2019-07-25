aca582648978e84968f5432d66aa8eca435d1714
Mitsuki Aihara <aizu.s1230001@gmail.com>
2019-07-25 18:15:56 +0900

    update User class

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,11 +1,29 @@
 export class User {
+    public readonly uid: string = '';
+    public name: string = '';
+    public imageURL: string = '';
+    public introduction: string = '';
+    public readonly createdAt: Date = new Date();
+    public updatedAt: Date = new Date();
+
     constructor(
-        public uid: string,
-        public email: string,
-        public password: string,
-        public name: string,
-        public icon: string,
-        public message: string,
-        public reservations: string[] = [],
-    ) {}
+        init?: Partial<User>
+
+        // public uid: string,
+        // public email: string,
+        // public password: string,
+        // public name: string,
+        // public icon: string,
+        // public message: string,
+        // public reservations: string[] = [],
+
+        // public uid: string,
+        // public name: string,
+        // public imageURL: string,
+        // public introduction: string,
+        // public createdAt: Date,
+        // public updatedAt: Date
+    ) {
+        Object.assign(this, init);
+    }
 }
