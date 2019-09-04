7f4432c7526904b9004607a5932f1d1303822570
aihara <aizu.s1230001@u-aizu.ac.jp>
2019-09-05 01:22:44 +0900

    fix the akabeko stoped on the left animation problem and move it in reverse

--- src/app/pages/taxi-reservation/taxi-reservation.page.ts
+++ src/app/pages/taxi-reservation/taxi-reservation.page.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./taxi-reservation.page.scss'],
 })
 export class TaxiReservationPage implements OnInit {
+  data_order = "left";
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  animationend(){
+    // console.log('animation is end');
+    this.data_order = this.data_order === "left" ? "right" : "left";
   }
 
 }
