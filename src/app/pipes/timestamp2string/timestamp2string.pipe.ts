7714a1efc4ae42802b5a4994a9c60b0ac98b5fb7
aihara <aizu.s1230001@u-aizu.ac.jp>
2019-09-04 18:02:50 +0900

    change display time format in reservation card

--- src/app/pipes/timestamp2string/timestamp2string.pipe.ts
+++ src/app/pipes/timestamp2string/timestamp2string.pipe.ts
@@ -8,15 +8,37 @@ export class Timestamp2stringPipe implements PipeTransform {
   transform(value: firebase.firestore.Timestamp, args?: any): any {
     // console.log('value:', value);
     // console.log('value.toDate():', value.toDate());
+    var today = new Date();
     var date = value.toDate();
+    // var date_str = [
+    //   date.getFullYear(),
+    //   date.getMonth() + 1,
+    //   date.getDate()
+    // ].join( '/' ) + ' '
+    // + date.toLocaleTimeString();
+
     var date_str = [
       date.getFullYear(),
       date.getMonth() + 1,
       date.getDate()
-    ].join( '/' ) + ' '
-    + date.toLocaleTimeString();
+    ].join( '/' );
 
-    return date_str;
+    var today_str = [
+      today.getFullYear(),
+      today.getMonth() + 1,
+      today.getDate()
+    ].join( '/' );
+
+    if (date_str === today_str) {
+      date_str = '本日';
+    }
+
+    var time_str = [
+      ('0' + date.getHours().toString()).slice(-2),
+      ('0' + date.getMinutes().toString()).slice(-2)
+    ].join( ':' );
+
+    return date_str + ' ' + time_str;
   }
 
 }
