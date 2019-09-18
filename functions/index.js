47f42814614b5d7765de0244c68ece2f815dcc9c
aihara <aizu.s1230001@u-aizu.ac.jp>
2019-09-19 01:51:30 +0900

    fix error in functions/index.js

--- functions/index.js
+++ functions/index.js
@@ -35,6 +35,8 @@ var fireStore = admin.firestore();
  * 使用例: https://us-central1-aisharing-ac6cc.cloudfunctions.net/routeSearch?route=30,139,21,140
  * リクエストパラメーター
  * {String} route 出発地と目的地の緯度経度。「出発地の緯度、出発地の経度、目的地の緯度、目的地の経度」
+ * valid_distance: 歩きで許容する距離
+ * max_count: 乗り換え回数
  * */ 
 exports.routeSearch = functions.https.onRequest(async (req, res) => {
     const query = req.query;
@@ -57,6 +59,12 @@ exports.routeSearch = functions.https.onRequest(async (req, res) => {
         lat: Number(route[2]),
         lng: Number(route[3])
     };
+
+    var valid_distance = query.valid_distance ? query.valid_distance : 10000;
+    var max_count = query.max_count ? query.max_count : 1;
+    var walk_speed = 80;  // 80m/m
+    var car_speed = 1000;  // 1000m/m
+
     // console.log('route:', route);
     // console.log('admin:', admin);
 
@@ -78,7 +86,7 @@ exports.routeSearch = functions.https.onRequest(async (req, res) => {
             departure_time: reservation.departure_time,
             fare: reservation.fare,
             total_distance: reservation.total_distance,
-            total_time: total_time,
+            total_time: reservation.total_time,
             departure_point: {
                 lat: reservation.departure_point.latitude,
                 lng: reservation.departure_point.longitude
@@ -111,7 +119,15 @@ exports.routeSearch = functions.https.onRequest(async (req, res) => {
         return d;
     };
 
+    /*
+    * valid_distance: 歩きで許容する距離
+    * max_count: 乗り換え回数
+    * walk_speed: 徒歩で移動するときの速度
+    * car_speed:
+    */
     fastreservations = (valid_distance, max_count, walk_speed, car_speed) =>{
+        // すでに出発した投稿は使えないようにする
+        // 同じ投稿を使えなくする
         responses = [];
         nows = [];
         reservations.forEach(reserve => {
@@ -119,7 +135,7 @@ exports.routeSearch = functions.https.onRequest(async (req, res) => {
             if(reserve.condition !== "募集中")return;
             if(distance <= valid_distance){
                 add = {
-                    logs: {reserve},
+                    logs: [reserve],
                     destination_point: reserve.destination_point,
                     total_time: reserve.total_time + distance / walk_speed,
                 };
@@ -127,8 +143,8 @@ exports.routeSearch = functions.https.onRequest(async (req, res) => {
             }
         });
         nows.sort((a,b)=>{
-            a_time = a.total_time + cul_distance(a.destination_point, destination_point);
-            b_time = b.total_time + cul_distance(b.destination_point, destination_point);
+            a_time = a.total_time + cal_distance(a.destination_point, destination_point);
+            b_time = b.total_time + cal_distance(b.destination_point, destination_point);
             return a_time - b_time;
         });
         while(nows.length>=10)nows.pop();
@@ -145,7 +161,8 @@ exports.routeSearch = functions.https.onRequest(async (req, res) => {
                     if(reserve.condition !== "募集中")return;
                     if(distance <= valid_distance){
                         add = {
-                            logs: now_target.logs.concat({reserve}),
+                            logs: now_target.logs.push(reserve),
+                            // logs: now_target.logs.concat({reserve}),  // Error: concatがありません
                             destination_point: reserve.destination_point,
                             total_time: reserve.total_time + distance / walk_speed + now_target.total_time,
                         };
@@ -155,8 +172,8 @@ exports.routeSearch = functions.https.onRequest(async (req, res) => {
             });
             nows = next_nows;
             nows.sort((a,b)=>{
-                a_time = a.total_time + cul_distance(a.destination_point, destination_point);
-                b_time = b.total_time + cul_distance(b.destination_point, destination_point);
+                a_time = a.total_time + cal_distance(a.destination_point, destination_point);
+                b_time = b.total_time + cal_distance(b.destination_point, destination_point);
                 return a_time - b_time;
             });
             while(nows.length>=10)nows.pop();
@@ -166,7 +183,11 @@ exports.routeSearch = functions.https.onRequest(async (req, res) => {
                 responses.push(now_target);
             });
         }
+        return responses;
     }
 
-    res.send({departure: departure_point, destination: destination_point, reservations: reservations, Number: distance});
+    var responses = fastreservations(valid_distance, max_count, walk_speed, car_speed);
+    console.log('responses:', responses);
+    // res.send({departure: departure_point, destination: destination_point, reservations: reservations, Number: distance, responses: responses});
+    res.send({responses: responses});
 });
